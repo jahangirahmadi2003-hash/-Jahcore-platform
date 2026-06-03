83a890d46b656acea872c8e9199cae32d7eef3c2
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-03 15:05:17 +0430

    Create sidebar.js

---
+++ sidebar.js
@@ -0,0 +1,10 @@
+function Sidebar() {
+  return `
+    <div class="sidebar">
+      <div>🏠 Home</div>
+      <div>📚 Library</div>
+      <div>🔍 Search</div>
+      <div>👤 Profile</div>
+    </div>
+  `;
+}
