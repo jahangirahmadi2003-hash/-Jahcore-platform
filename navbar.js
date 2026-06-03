0bab103cc9bf93fc5ad584fa31788f315aa8a4cd
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-03 15:04:47 +0430

    Create navbar.js

---
+++ navbar.js
@@ -0,0 +1,11 @@
+function Navbar() {
+  return `
+    <div class="navbar">
+      <div class="logo">⚡ JAHCORE</div>
+      <div>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </div>
+  `;
+}
