432ea5ef8820388e3c0785f1c9800b692c6c5e3d
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-04 05:49:20 +0430

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-function Navbar(user) {
-  return `
-    <div class="navbar">
-      <div class="logo">⚡ JAHCORE</div>
-
-      <div>
-        ${
-          user
-            ? `<span>👤 ${user.email}</span> <button onclick="logout()">Logout</button>`
-            : `
-              <input id="email" placeholder="email" />
-              <input id="pass" type="password" placeholder="password" />
-
-              <button onclick="register(
-                document.getElementById('email').value,
-                document.getElementById('pass').value
-              )">Register</button>
-
-              <button onclick="login(
-                document.getElementById('email').value,
-                document.getElementById('pass').value
-              )">Login</button>
-            `
-        }
-      </div>
-    </div>
-  `;
-}
+document.getElementById("app").innerHTML = `
+  <div style="
+    padding:40px;
+    font-family:Arial;
+    text-align:center;
+  ">
+    <h1>⚡ JAHCORE</h1>
+    <p>Platform is running successfully.</p>
+    <p>مرحله اول با موفقیت اجرا شد.</p>
+  </div>
+`;
