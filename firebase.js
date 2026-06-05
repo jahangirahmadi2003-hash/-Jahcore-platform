91fc87c88e252c3390daf38ac5849632269eeb4d
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-05 06:01:08 +0430

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,18 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+// ⚠️ اینجا config خودت را از Firebase بگذار
 const firebaseConfig = {
-  apiKey: "AIzaSyDrbEyAFk_hJmBdU1GIaUTW5mZWCL4vXrM",
-  authDomain: "jahcore-e8653.firebaseapp.com",
-  databaseURL: "https://jahcore-e8653-default-rtdb.firebaseio.com",
-  projectId: "jahcore-e8653",
-  storageBucket: "jahcore-e8653.appspot.com",
-  messagingSenderId: "869095509266",
-  appId: "1:869095509266:web:1e3ba92bfc3a01ac1e77cb"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 const app = initializeApp(firebaseConfig);
 
-// اتصال به Realtime Database
-export const db = getDatabase(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
