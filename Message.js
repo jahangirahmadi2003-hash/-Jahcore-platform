8593a7afaf7b9ce88e95ca6cf6eb4fc53663f976
jahangirahmadi2003-hash <jahangirahmadi2003@gmail.com>
2026-06-07 05:14:35 +0430

    Create Message.js

---
+++ Message.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+  conversationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Conversation',
+    required: true
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  receiver: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  attachments: [
+    {
+      type: String, // URL
+      fileType: String
+    }
+  ],
+  isRead: {
+    type: Boolean,
+    default: false
+  },
+  readAt: {
+    type: Date,
+    default: null
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  edited: {
+    type: Boolean,
+    default: false
+  },
+  editedAt: {
+    type: Date,
+    default: null
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
